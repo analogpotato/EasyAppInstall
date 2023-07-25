6626c8291a3331c98e1a57335e87d6b56106caf1
Frank Foster <frank.e.foster@icloud.com>
2023-07-24 22:17:42 -0700

    added further prompts

--- chocolatey/install.ps1
+++ chocolatey/install.ps1
@@ -14,7 +14,7 @@ catch {
 
 # Step 2: Download a file from GitHub
 $defaultUrl = "https://raw.githubusercontent.com/analogpotato/EasyAppInstall/main/chocolatey/packages.txt"
-$userUrl = Read-Host -Prompt "Enter the URL to the packages file (or press Enter to use the default URL)"
+$userUrl = Read-Host -Prompt "Enter the URL to the packages file (or press Enter to use the default URL which is located at: https://raw.githubusercontent.com/analogpotato/EasyAppInstall/main/chocolatey/packages.txt)"
 $Url = if ($userUrl) { $userUrl -replace "github\.com", "raw.githubusercontent.com" -replace "/blob/", "/" } else { $defaultUrl }
 $output = "$env:temp\packages.txt"
 Start-BitsTransfer -Source $Url -Destination $output
