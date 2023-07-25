b22004f2747bfcb07defe6fbc3ab4997dee29eb1
Frank Foster <frank.e.foster@icloud.com>
2023-07-24 23:54:21 -0700

    reversion

--- brew/install.sh
+++ brew/install.sh
@@ -1,33 +1,26 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Step 1: Check if Homebrew is installed
 if ! command -v brew &> /dev/null
 then
     echo "❌ Homebrew not found. Installing now..."
-    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     echo "Homebrew is already installed ✅"
 fi
 
-# Step 2: Download the list of packages from GitHub
-url="https://raw.githubusercontent.com/analogpotato/EasyAppInstall/main/brew/packages.txt"  # replace this with your file URL
-output="/tmp/packages.txt"
-curl -L $url -o $output
-
-# Read the file into an array
-packages=()
-while IFS= read -r line || [[ -n "$line" ]]; do
-    packages+=("$line")
-done < "$output"
+# Step 2: Define the list of packages
+packages=("visual-studio-code" "vlc" "spotify")
 
 # Step 3: Install the packages
 echo "🛠️ Installing packages..."
 for package in "${packages[@]}"
 do
     if brew list | grep -q "^${package}\$"; then
-        echo "$package is already installed with Homebrew ✅"
+        echo "$package is already installed ✅"
     else
         echo "🛠️ Installing $package..."
         brew install "$package" || echo "❌ Failed to install $package"
     fi
 done
+
