1bf9be1fac32becb3056fa2b0aa322106f7ed866
Frank Foster <frank.e.foster@icloud.com>
2023-07-24 21:10:07 -0700

    testing

--- chocolatey/install.ps1
+++ chocolatey/install.ps1
@@ -6,11 +6,11 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Step 2: Download a file from GitHub
-$Url = "https://raw.githubusercontent.com/your_username/your_repository/master/your_file.txt"  # replace this with your file URL
-$output = "$env:temp\your_file.txt"
+$Url = "https://raw.githubusercontent.com/analogpotato/chocolatey/packages.txt"  # replace this with your file URL
+$output = "$env:temp\package.txt"
 Invoke-WebRequest -Uri $Url -OutFile $output
 
-# Step 3: Read the contents of the file and install the packages
-Get-Content $output | ForEach-Object {
-    choco install $_ -y  # the -y flag automatically confirms all prompts
-}
+# # Step 3: Read the contents of the file and install the packages
+# Get-Content $output | ForEach-Object {
+#     choco install $_ -y  # the -y flag automatically confirms all prompts
+# }
