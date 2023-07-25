91b1c27d6fd59c6d8993a84d1ebafd024a6298f2
Frank Foster <frank.e.foster@icloud.com>
2023-07-24 22:42:12 -0700

    added brew

---
+++ brew/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Step 1: Check if Homebrew is installed
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew not found. Installing now..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed"
+fi
+
+# Step 2: Download a file from GitHub
+default_url="https://raw.githubusercontent.com/analogpotato/EasyAppInstall/main/brew/packages.txt"
+echo "Enter the URL to the packages file (or press Enter to use the default URL):"
+read user_url
+url=${user_url:-$default_url}
+
+output="/tmp/packages.txt"
+curl -L $url -o $output
+
+# Step 3: Read the contents of the file and install the packages
+while IFS= read -r package
+do
+  brew install "$package"
+done < "$output"
