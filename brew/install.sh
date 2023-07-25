73c01aa4cd77153ed1a92a1c61d07182b7eb81a1
Frank Foster <frank.e.foster@icloud.com>
2023-07-25 00:30:18 -0700

    updated bash

--- brew/install.sh
+++ brew/install.sh
@@ -9,12 +9,32 @@ else
     echo "Homebrew is already installed ✅"
 fi
 
-# Step 2: Define the list of packages
-packages=("visual-studio-code" "vlc" "spotify")
+# Step 2: Get the location of the packages file
+echo "Enter the URL or local path to the packages file (or press Enter to use the default URL which is located at: https://raw.githubusercontent.com/analogpotato/EasyAppInstall/main/brew/packages.txt)"
+read user_input
+if [ -z "$user_input" ]
+then
+    # No input, use the default URL
+    url="https://raw.githubusercontent.com/analogpotato/EasyAppInstall/main/brew/packages.txt"
+    output="/tmp/packages.txt"
+    curl -L $url -o $output
+elif [[ $user_input == http* ]]
+then
+    # Input is a URL, download the file
+    url=$user_input
+    output="/tmp/packages.txt"
+    curl -L $url -o $output
+else
+    # Input is a local path
+    output=$user_input
+fi
 
-# Step 3: Install the packages
+# Convert Windows line endings to Unix line endings
+sed -i '' 's/\r$//' "$output"
+
+# Step 3: Read the contents of the file and install the packages
 echo "🛠️ Installing packages..."
-for package in "${packages[@]}"
+while IFS= read -r package
 do
     if brew list | grep -q "^${package}\$"; then
         echo "$package is already installed ✅"
@@ -22,5 +42,4 @@ do
         echo "🛠️ Installing $package..."
         brew install "$package" || echo "❌ Failed to install $package"
     fi
-done
-
+done < "$output"
