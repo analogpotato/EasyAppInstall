ee3dc2f25d71d9ff69ecfb50eb829e68cbab6bdc
Frank Foster <frank.e.foster@icloud.com>
2023-07-24 21:03:01 -0700

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # EasyAppInstall
 This repo will use either chocolatey or brew to install a series of favorite apps from a package manager
+
+
+This command should run the powershell:
+
+$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/analogpotato/EasyAppInstall/chocolatey/install.ps1
+Invoke-Expression $($ScriptFromGitHub.Content)
---
+++ chocolatey/install.ps1
@@ -0,0 +1,16 @@
+# This script requires PowerShell version 3 or above
+
+# Step 1: Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Step 2: Download a file from GitHub
+$Url = "https://raw.githubusercontent.com/your_username/your_repository/master/your_file.txt"  # replace this with your file URL
+$output = "$env:temp\your_file.txt"
+Invoke-WebRequest -Uri $Url -OutFile $output
+
+# Step 3: Read the contents of the file and install the packages
+Get-Content $output | ForEach-Object {
+    choco install $_ -y  # the -y flag automatically confirms all prompts
+}
